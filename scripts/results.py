507e3ef9727b2fc0e7a86b05835bdcf2b5abef92
Deeraj Nachimuthu <nachimuthu.d@northeastern.edu>
2022-05-06 17:30:44 -0400

    minor change

--- scripts/results.py
+++ scripts/results.py
@@ -151,15 +151,6 @@ if len(sys.argv) == 2:
         ax[0].annotate(txt, (X[i]+0.05, f1_score_lib[i]+0.01))
         ax[1].annotate(txt, (X[i]+0.05, f1_score_cons[i]+0.01))
 
-    print(test_overall_accuracy)
-    print(liberal_accuracy)
-    print(conservative_accuracy)
-    print(recall_lib)
-    print(precision_lib)
-    print(f1_score_lib)
-    print(recall_cons)
-    print(precision_cons)
-    print(f1_score_cons)
     plt.show()
 else:
     raise Exception('not enough arguments to run the script.\n'
